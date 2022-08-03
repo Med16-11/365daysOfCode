2cbe3eeb2f639b9b75a4f85b4f583711e1c57954
Medhavi Singh <78318301+Med16-11@users.noreply.github.com>
2022-08-03 23:31:02 +0530

    Create  Merge Sorted Array.cpp

---
+++ DSA_Problems/Arrays/Merge Sorted Array.cpp
@@ -0,0 +1,22 @@
+ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        int k=nums1.size()-1;
+        int i=m-1,j=n-1;
+        while(i>=0 &&j>=0){
+            if(nums2[j]>nums1[i]){
+                nums1[k]=nums2[j];
+                k--;
+                j--;
+            }else{
+                nums1[k]=nums1[i];
+                k--;
+                i--;
+            }
+        }
+        while(i>=0){
+            nums1[k--]=nums1[i--];
+        }
+        while(j>=0){
+            nums1[k--]=nums2[j--];
+        }
+    }
