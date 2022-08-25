64473b772be4df1cf6147282918752acf3f171d1
Medhavi Singh <78318301+Med16-11@users.noreply.github.com>
2022-08-25 23:32:30 +0530

    Create aug25.cpp

---
+++ DSA_Problems/LeetCodeDaily/aug25.cpp
@@ -0,0 +1,17 @@
+//Ransom Note
+bool canConstruct(string ransomNote, string magazine) {
+        
+        unordered_map<char, int> m;
+        
+        for(auto x: magazine){
+            m[x]++;
+        }
+        
+        for(auto x: ransomNote){
+            if(!m[x]--){
+                return false;
+            }
+        }
+        
+        return true;
+    }
