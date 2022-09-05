ee38e450f9814c8fd5cd36c8cdecd79448675d78
Medhavi Singh <78318301+Med16-11@users.noreply.github.com>
2022-09-05 13:36:46 +0530

    Create aug5.cpp

---
+++ DSA_Problems/LeetCodeDaily/aug5.cpp
@@ -0,0 +1,31 @@
+vector<vector<int>> levelOrder(Node* root) {
+        
+        if(root == NULL){
+            return {};
+        }
+        
+        queue<Node*>q;
+        q.push(root);
+        
+        vector<vector<int>>ans;
+        
+        while(!q.empty()){
+            
+            int n = q.size();
+            vector<int>v;
+        
+            for(int i = 0; i < n; i++){
+                Node * temp = q.front();
+                q.pop();
+        
+                for(auto child: temp->children){
+                    q.push(child);
+                }
+        
+                v.push_back(temp-> val);
+            }   
+        
+            ans.push_back(v);
+        }
+        return ans;
+    }
