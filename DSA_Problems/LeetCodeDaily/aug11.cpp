9435677c7c3bc6c2dfcf57dd7a5af451786e81b8
Medhavi Singh <78318301+Med16-11@users.noreply.github.com>
2022-09-11 19:50:34 +0530

    Create aug11.cpp

---
+++ DSA_Problems/LeetCodeDaily/aug11.cpp
@@ -0,0 +1,22 @@
+// Maximum Performance of a Team
+int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
+        
+        vector<pair<int, int>>v;
+        priority_queue<int, vector<int>, greater<int>>pq;
+        for(int i =0; i<n; i++){
+            v.push_back({efficiency[i], speed[i]});
+        }
+        long sum = 0, ans = 0;
+        sort(v.begin(), v.end());
+        for(auto i = n-1; i >=0; i-- ){
+            sum += v[i].second;
+            pq.push(v[i].second);
+            if(pq.size() > k){
+                sum -= pq.top();
+                pq.pop();
+            }
+            ans = max(sum*v[i].first, ans);
+        }
+        
+        return ans%1000000007;
+    }
