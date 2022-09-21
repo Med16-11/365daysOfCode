3dfc241ce904fa6ba65a6dfea07e8eb43dc250c0
Medhavi Singh <78318301+Med16-11@users.noreply.github.com>
2022-09-21 22:38:12 +0530

    Create RightSideView.cpp

---
+++ DSA_Problems/BinaryTree/RightSideView.cpp
@@ -0,0 +1,12 @@
+void rightview(TreeNode* root, int level, vector<int>&ans){
+        if(root == NULL) return;
+        if(ans.size() == level) ans.push_back(root->val);
+        rightview(root->right, level+1, ans);
+        rightview(root->left, level+1, ans);
+        
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans;
+        rightview(root, 0, ans);
+        return ans;
+    }
