9c4137347906f42f233ccb7b154951b62ddaa2a9
Medhavi Singh <78318301+Med16-11@users.noreply.github.com>
2022-09-23 21:55:29 +0530

    Create sept23.cpp

---
+++ DSA_Problems/LeetCodeDaily/sept23.cpp
@@ -0,0 +1,12 @@
+//Concatenation of Consecutive Binary Numbers
+ int concatenatedBinary(int n) {
+        
+        long long int val = 0;
+        int i = 1;
+    while(i<=n){
+        val = ((val<<(1+int(log2(i))))%1000000007+i)%1000000007;
+        i++;
+    }   
+        return val;
+    }
+};
