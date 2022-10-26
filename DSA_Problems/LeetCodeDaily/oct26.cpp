6452cc09ca3a007eb1655c5cba7326302ceb0a66
Medhavi Singh <78318301+Med16-11@users.noreply.github.com>
2022-10-26 21:17:15 +0530

    Create oct26.cpp

---
+++ DSA_Problems/LeetCodeDaily/oct26.cpp
@@ -0,0 +1,16 @@
+//Continuous Subarray Sum
+bool checkSubarraySum(vector<int>& nums, int k) {
+        int prefSum = 0;
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++){
+            prefSum += nums[i];
+            prefSum %= k;
+            if(prefSum == 0 && i) return true;
+
+            if(mp.find(prefSum) != mp.end()) {
+                if(i - mp[prefSum] > 1) return true; // check if atleast 2 elements are there or not
+            }
+            else mp[prefSum] = i;
+        }
+        return false; 
+    }
