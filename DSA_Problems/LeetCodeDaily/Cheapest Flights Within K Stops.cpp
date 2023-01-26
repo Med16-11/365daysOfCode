0ce2ad4e1a643ce5f6caba3efa2e9a28b5a638a0
Medhavi Singh <78318301+Med16-11@users.noreply.github.com>
2023-01-26 21:50:30 +0530

    Create Cheapest Flights Within K Stops.cpp

---
+++ DSA_Problems/LeetCodeDaily/Cheapest Flights Within K Stops.cpp
@@ -0,0 +1,27 @@
+int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+       vector<vector<pair<int, int>>> adj(n);
+        for(auto flight : flights){
+            adj[flight[0]].push_back({flight[1], flight[2]});
+        }
+        queue<pair<int, int>> q;
+        q.push({src, 0});
+        vector<int> minCost(n, INT_MAX);
+        int stops = 0;
+        while(!q.empty() && stops <= k){
+            int size = q.size();
+            while (size--) {
+                auto [currNode, cost] = q.front();
+                q.pop();
+                for (auto& [neighbour, price] : adj[currNode]) {
+                    if (price + cost < minCost[neighbour]){
+                        minCost[neighbour] = price + cost;
+                        q.push({neighbour, minCost[neighbour]});
+                    }
+                }
+            }
+            stops++;
+        }
+        if(minCost[dst] == INT_MAX)
+            return -1;
+        return minCost[dst]; 
+    }
