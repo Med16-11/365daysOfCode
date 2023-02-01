4fd17143817c2d08926538413d0d008d8f521343
Medhavi Singh <78318301+Med16-11@users.noreply.github.com>
2023-02-01 22:03:44 +0530

    Create Greatest Common Divisor of Strings.cpp

---
+++ DSA_Problems/LeetCodeDaily/Greatest Common Divisor of Strings.cpp
@@ -0,0 +1,6 @@
+string gcdOfStrings(string str1, string str2) {
+        if(str1 + str2 == str2 + str1){
+            return str1.substr(0, gcd(str1.length(), str2.length()));
+        }
+        return "";
+    }
