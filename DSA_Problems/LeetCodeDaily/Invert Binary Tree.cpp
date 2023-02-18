c3f5e4f82ca4ae11160857fd597aae1e22533e97
Medhavi Singh <78318301+Med16-11@users.noreply.github.com>
2023-02-18 22:33:40 +0530

    Create Invert Binary Tree.cpp

---
+++ DSA_Problems/LeetCodeDaily/Invert Binary Tree.cpp
@@ -0,0 +1,8 @@
+TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL) return root;
+       swap(root->left, root->right);
+       invertTree(root->left);
+       invertTree(root->right);
+
+       return root;
+    }
