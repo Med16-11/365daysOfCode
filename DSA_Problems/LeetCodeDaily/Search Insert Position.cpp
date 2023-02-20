e5f3c6d96041f85000b01d7dabd8b56338c99688
Medhavi Singh <78318301+Med16-11@users.noreply.github.com>
2023-02-20 19:57:24 +0530

    Create Search Insert Position.cpp

---
+++ DSA_Problems/LeetCodeDaily/Search Insert Position.cpp
@@ -0,0 +1,20 @@
+int low=0;
+        int high=nums.size();
+        int mid;
+        if(target>nums[high-1]){
+            return high;
+        }
+        while(low<=high){
+              mid=(low+high)/2;
+            if(nums[mid]==target){  //if found return its position
+                return mid;
+            }
+          
+            if(target<nums[mid]){     
+            high=mid-1;    
+            }else{
+            low=mid+1;        
+            }
+          
+        }
+         return  low;
