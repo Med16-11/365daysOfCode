82657a6370b724cf5e4657b91fee49734a0504e1
Medhavi Singh <78318301+Med16-11@users.noreply.github.com>
2023-07-25 23:56:04 +0530

    Update Peak Index in a Mountain Array.cpp

--- DSA_Problems/LeetCodeDaily/Peak Index in a Mountain Array.cpp
+++ DSA_Problems/LeetCodeDaily/Peak Index in a Mountain Array.cpp
@@ -1,1 +1,15 @@
-
+nt peakIndexInMountainArray(vector<int>& arr) {
+        int left = 0;
+        int right = arr.size() - 1;
+        int mid;
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+        
+    }
