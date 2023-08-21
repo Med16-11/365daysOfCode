e08a0e3c2b788e41ce2435e0e736b29974c82b00
Medhavi Singh <78318301+Med16-11@users.noreply.github.com>
2023-08-22 00:59:41 +0530

    Create Valid Parentheses.cpp

---
+++ DSA_Problems/TOP50interviewQuestions/Stacks/Valid Parentheses.cpp
@@ -0,0 +1,17 @@
+bool isValid(string s) {
+        stack<char>st;
+        for(char c : s){
+            if( c == '(' || c == '{' || c == '['){
+                st.push(c);
+            }else{
+                if(st.empty() || 
+                    (c == ')' && st.top() != '(') ||
+                    (c == '}' && st.top() != '{') ||
+                    (c == ']' && st.top() != '[')){
+                    return false;  
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
